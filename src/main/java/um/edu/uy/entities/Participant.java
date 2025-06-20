eda77d148fa595a5857fd9249baf26bb1e93d790
juanigzangaro <juanigzangaro@gmail.com>
2025-06-19 21:01:31 -0300

    Actor, Participant, Person y CreditsLoader

--- src/main/java/um/edu/uy/entities/Participant.java
+++ src/main/java/um/edu/uy/entities/Participant.java
@@ -1,4 +1,33 @@
 package um.edu.uy.entities;
 
-public class Participant {
+import um.edu.uy.importer.CreditsLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Participant extends Person {
+    private List<CrewParticipation> crewActivity;
+
+    public Participant() {
+        super();
+        this.crewActivity = new ArrayList<>();
+    }
+    public Participant(String personId, String name, String creditId) {
+        super(personId, name, creditId);
+        this.crewActivity = new ArrayList<>();
+    }
+
+    public List<CrewParticipation> getCrewActivity() {
+        return crewActivity;
+    }
+
+    public void setCrewActivity (List<CrewParticipation> crewActivity) {
+        this.crewActivity = crewActivity;
+
+    }
+
+    public void addCrewParticipation(CrewParticipation participation) {
+        this.crewActivity.add(participation);
+    }
+
 }
