4250472b66c7ec80ca9e26f569f5c5f237cb95fc
rosariostoppiello <rstoppiello@correo.um.edu.uy>
2025-06-27 20:52:23 -0300

    Pequeño cambio de consistencia en la forma en la que se imprimen las opciones del menu de consultas

--- src/main/java/um/edu/uy/Main.java
+++ src/main/java/um/edu/uy/Main.java
@@ -40,13 +40,13 @@ public class Main {
                 consultas:
                 while (true) {
                     System.out.println("Menú consultas: ");
-                    System.out.println("1. Top 5 de las películas que más calificaciones por idioma.");
+                    System.out.println("1. Top 5 de las películas con más calificaciones por idioma.");
                     System.out.println("2. Top 10 de las películas que mejor calificación media tienen por parte de los usuarios.");
                     System.out.println("3. Top 5 de las colecciones que más ingresos generaron.");
                     System.out.println("4. Top 10 de los directores que mejor calificación tienen.");
                     System.out.println("5. Actor con más calificaciones recibidas en cada mes del año.");
-                    System.out.println("6. Usuarios con más calificaciones por género");
-                    System.out.println("7. Salir");
+                    System.out.println("6. Usuarios con más calificaciones por género.");
+                    System.out.println("7. Salir.");
 
                     while (!scanner.hasNextInt()) {
                         System.out.println("Opción inválida. Por favor, ingrese un número.");
